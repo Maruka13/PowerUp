480f2bb69745e829cf8e9f4dd2902012a7b7d109
Maruka <emanuelle_carollina@hotmail.com>
2026-01-12 20:14:00 -0300

    lógica

---
+++ codigo.py
@@ -0,0 +1,5 @@
+# passo 1: entrar no sistema da empresa 
+# passo 2: fazer login
+# passo 3: abrir base de dados
+# passo 4: cadastrar um produto
+# passo 5: repetir o passo 4 ate acabar a lista de produtos
