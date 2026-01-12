21e86b27e49dd4d17b43093a3daa02ae99976193
Maruka <emanuelle_carollina@hotmail.com>
2026-01-12 20:58:44 -0300

    definição de link para login

--- codigo.py
+++ codigo.py
@@ -8,6 +8,12 @@ import pyautogui
 
 pyautogui.PAUSE = 0.5
 
+link = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
+
 pyautogui.press("win")       # pyautogui.hotkey("command", "space") # macOS
 pyautogui.write("chrome")
 pyautogui.press("enter")
+
+pyautogui.write(link)
+pyautogui.press("enter")
+
