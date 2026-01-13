7b9f553049c47f9854881d07b507f2120aad0e85
Maruka <emanuelle_carollina@hotmail.com>
2026-01-12 23:06:06 -0300

    implementar preenchimento dos campos de cadastro de produto

--- codigo.py
+++ codigo.py
@@ -27,10 +27,36 @@ pyautogui.press("tab") # pula para senha
 pyautogui.write("senha deveras dificil")
 pyautogui.press("tab") # pula pro botao
 pyautogui.press("enter")
-time.sleep(5)
+time.sleep(3)
 
 # pip install pandas openpyxl
 
 import pandas
 
 tabela = pandas.read_csv("produtos.csv")
+
+pyautogui.click(x=671, y=258) # clicar em codigo do produto
+
+pyautogui.write("MOLO000251")
+pyautogui.press("tab")
+
+pyautogui.write("Logitech")
+pyautogui.press("tab")
+
+pyautogui.write("Mouse")
+pyautogui.press("tab")
+
+pyautogui.write("Categoria")
+pyautogui.press("tab")
+
+pyautogui.write("Preco")
+pyautogui.press("tab")
+
+pyautogui.write("Custo")
+pyautogui.press("tab")
+
+pyautogui.write("Obs")
+pyautogui.press("tab")
+
+pyautogui.press("enter")
+
